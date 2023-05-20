b5a28948f16010bbe1dda71fe76d58d9070c9d21
Sergey T <tolstikovsn@gmail.com>
2023-05-20 21:48:00 +0500

    delete reducer del msgs with channel

--- frontend/src/store/slices/messagesSlice.js
+++ frontend/src/store/slices/messagesSlice.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+import { removeChannel } from './channelsSlice';
 
 const initialState = {
   messages: [],
@@ -15,6 +16,14 @@ const messagesSlice = createSlice({
     addMessage: (state, action) => {
       state.messages.push(action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(removeChannel, (state, action) => {
+        const { id } = action.payload;
+        const restMessages = state.messages.filter((message) => message.channelId !== id);
+        state.messages = restMessages;
+      });
   },
 });
 
