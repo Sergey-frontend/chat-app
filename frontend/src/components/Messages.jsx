efcc166682feaa019bf06a70a7b307e7749f68f9
Sergey T <tolstikovsn@gmail.com>
2023-05-24 14:48:13 +0500

    Fixed text overflow in messages

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,4 +1,6 @@
-import { Col } from 'react-bootstrap';
+import {
+  Col, Form, InputGroup, Button,
+} from 'react-bootstrap';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -34,7 +36,7 @@ const Messages = () => {
     );
   });
 
-  const formHendler = (e) => {
+  const formHandler = (e) => {
     e.preventDefault();
     e.target.value = '';
     const formData = new FormData(e.target);
@@ -50,7 +52,7 @@ const Messages = () => {
   };
 
   return (
-    <Col className="col-9 p-0 h-100">
+    <Col className="col-9 p-0 d-flex flex-column h-100">
       <div className="bg-light mb-4 p-3 shadow-sm small">
         <p className="m-0">
           <b>
@@ -59,50 +61,35 @@ const Messages = () => {
             {currentChannel ? currentChannel.name : t('messages.loading')}
           </b>
         </p>
-        <span className="text-muted">
-          {t('messagesCount.key', { count })}
-        </span>
+        <span className="text-muted">{t('messagesCount.key', { count })}</span>
       </div>
 
-      <div id="messages-box" className="chat-messages overflow-auto px-5 " style={{ height: '60vh' }}>
+      <div className="flex-grow-1 overflow-auto px-5" id="messages-box">
         {messagesList}
       </div>
 
-      <div className="mt-auto px-5">
-        <form
-          onSubmit={formHendler}
-          noValidate=""
-          className="py-1
-        border rounded-2"
-        >
-          <div className="input-group has-validation">
-            <input
+      <div className="mt-auto px-5 mb-2">
+        <Form onSubmit={formHandler} noValidate className="py-1 border rounded-2">
+          <InputGroup hasValidation>
+            <Form.Control
               onChange={controlInput}
               name="body"
               aria-label={t('messages.label')}
               placeholder={t('messages.placeholder')}
-              className="border-0 p-0 ps-2 form-control"
+              className="border-0 p-0 ps-2"
               value={inputValue}
             />
-            <button type="submit" className="btn btn-group-vertical" disabled="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                width="20"
-                height="20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                />
+            <Button type="submit" disabled={!inputValue} variant="outline-secondary">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+                <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
               </svg>
               <span className="visually-hidden">Отправить</span>
-            </button>
-          </div>
-        </form>
+            </Button>
+          </InputGroup>
+        </Form>
       </div>
     </Col>
+
   );
 };
 
